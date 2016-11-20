64ea107f18d6d99e6ea746bbf97fd75f30dafffa
decauwsemaecker.glen@gmail.com <decauwsemaecker.glen@gmail.com>
2016-11-20 11:17:34 -0500

    improve layout and structure

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,8 @@
 function initGiftList() {
     var items = [
         {
-            sum: "Envoltura de Bebé (Gris), para cargar al bebé",
+            sum: "Envoltura de Bebé (Gris)",
+            desc: "para cargar al bebé",
             price: 210,
             where: [
                 {name: "amazon", url: "https://www.amazon.com/Boba-BW1-005-Gray-Baby-Wrap-Grey/dp/B005SP2LWW"},
@@ -16,47 +17,58 @@ function initGiftList() {
             ],
         },
         {
-            sum: "Cuna para Bebé o Cuna portátil",
+            sum: "Cuna para Bebé",
+            desc: "o Cuna portátil",
             price: 950,
             where: [
                 {name: "falabella", url: "http://www.falabella.com.pe/falabella-pe/product/14661786/Mini-Cuna-para-Bebe-4040180"},
             ],
         },
         {
-          sum: "Móvil de colores y alegre para colgar encima de la cuna",
-          price: 250,
+            sum: "Móvil de colores",
+            desc: "y alegre para colgar encima de la cuna",
+            price: 250,
         },
         {
-          sum: "Ropita de bebé: calcetines, bodies de algodón, gorritos y/o conjuntitos de algodón",
-          price: 40,
+            sum: "Ropita de bebé",
+            desc: "calcetines, bodies de algodón, gorritos y/o conjuntitos de algodón",
+            price: 40,
         },
         {
-          sum: "Para la cuna: Mantitas de algodón o de pura lana virgen o de hilo; o almohada y cojines para recién nacido; o un juego de sábanas para la cuna; o un juego de protector y colcha para cuna",
-          price: 80,
+            sum: "Para la cuna",
+            desc: "Mantitas de algodón o de pura lana virgen o de hilo; o almohada y cojines para recién nacido; o un juego de sábanas para la cuna; o un juego de protector y colcha para cuna",
+            price: 80,
         },
         {
-          sum: "Paquetes de pañales y/o pañales de tela para 3kg-5kg",
-          price: 50 ,
+            sum: "Paquetes de pañales",
+            desc: "y/o pañales de tela para 3kg-5kg",
+            price: 50,
         },
         {
-          sum: "Ropita para bebé: pijamas, zapatitos o patucos y guantes de algodón y/o lana, peleles, conjuntos para paseo",
-          price: 80,
+            sum: "Ropita para bebé",
+            desc: "pijamas, zapatitos o patucos y guantes de algodón y/o lana, peleles, conjuntos para paseo",
+            price: 80,
         },
         {
-          sum: "Bolsa para llevar cambios, pañales, etc",
-          price: 60,
+            sum: "Bolsa",
+            desc: "para llevar cambios, pañales, etc...",
+            price: 60,
         },
         {
-          sum: "Intercomunicador de sonido",
-          price: 80,
+            sum: "Intercomunicador de sonido",
+            price: 80,
         },
         {
-          sum: "Extractor de leche",
-          price: 80,
+            sum: "Extractor de leche",
+            price: 80,
         },
     ];
 
     var content = "<ul>";
+
+    // sort based on desc. prices
+    items.sort(function(a, b){return b.price - a.price});
+
 
     for (var i = 0; i < items.length; i++) {
         var item = items[i];
